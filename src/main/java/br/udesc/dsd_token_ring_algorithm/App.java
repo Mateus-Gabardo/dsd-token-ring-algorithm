22a7f3149033bb450b6d04ebc08d6812033b1974
Mateus-Gabardo <92892326+Mateus-Gabardo@users.noreply.github.com>
2023-06-27 19:58:24 -0300

    Fist commit

---
+++ src/main/java/br/udesc/dsd_token_ring_algorithm/App.java
@@ -0,0 +1,13 @@
+package br.udesc.dsd_token_ring_algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
